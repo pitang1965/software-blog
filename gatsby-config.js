30b2046b18cdef55cbe2cd767fb2619e8dc1efc1
Shoji Makino <pitang1965@gmail.com>
2021-06-29 20:50:15 +0900

    Fix: invalid icon file names

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,13 +84,13 @@ module.exports = {
             src: 'content/assets/maskable-yama192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'maskable'
+            purpose: 'maskable',
           },
           {
             src: 'content/assets/maskable-yama512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'maskable'
+            purpose: 'maskable',
           },
         ],
       },
