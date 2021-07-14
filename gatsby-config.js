9ec69d60fb1b60ddcddf462a4f41402b76179e67
Shoji Makino <pitang1965@gmail.com>
2021-07-14 21:20:56 +0900

    Add gatsby-plugin-google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,13 +84,13 @@ module.exports = {
             src: 'content/assets/maskable-yama192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'maskable'
+            purpose: 'maskable',
           },
           {
             src: 'content/assets/maskable-yama512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'maskable'
+            purpose: 'maskable',
           },
         ],
       },
@@ -107,6 +107,50 @@ module.exports = {
       resolve: `gatsby-plugin-offline`,
       options: {
         precachePages: [`/self-introduction/`, `/portfolio/`],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'G-3RW979YMBJ',
+
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+
+        // Setting this parameter is optional
+        anonymize: true,
+
+        // Setting this parameter is also optional
+        respectDNT: true,
+
+        // Avoids sending pageview hits from custom paths
+        // exclude: ['/preview/**', '/do-not-track/me/too/'],
+
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+
+        // Enables Google Optimize using your container Id
+        // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+
+        // Enables Google Optimize Experiment ID
+        // experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
+
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: '0',
+
+        // Defers execution of google analytics script after page load
+        defer: false,
+
+        // Any additional optional fields
+        // sampleRate: 5,
+
+        siteSpeedSampleRate: 10,
+
+        // cookieDomain: 'example.com',
+
+        // defaults to false
+        enableWebVitalsTracking: true,
       },
     },
   ],
