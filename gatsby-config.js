80bab5ee88736cb1329c800a9149122d81ee97b1
Shoji Makino <pitang1965@gmail.com>
2021-07-16 21:10:10 +0900

    Fix: invalid trackingIds

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,7 +114,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "3RW979YMBJ", // Google Analytics / GA
+          "G-3RW979YMBJ", // Google Analytics / GA
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
